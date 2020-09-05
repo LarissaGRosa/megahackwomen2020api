5e8d440e8c12e904a5e3bd651c0da0bac83ff779
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2020-09-05 15:53:31 -0300

    Allow debug

--- megahackproject/settings.py
+++ megahackproject/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-2n^l)k&m2nk0&c9thckahj$syf@jas(0ri0(lf2tna1!r0ik%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['larigr.pythonanywhere.com']
 
