525f613c6316d7f899c321a56b5e0559a33df4ec
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2020-09-05 16:11:36 -0300

    Atualizando as configurações/Update settings configuration 4

--- megahackproject/settings.py
+++ megahackproject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = ['larigr.pythonanywhere.com']
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
+    'rest_framework.authtoken',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
