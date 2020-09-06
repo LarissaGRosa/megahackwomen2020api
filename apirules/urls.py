5dc94acae24a1d7d924026d3c6ff691b742a6d6a
Larissa Gremelmaier Rosa <larissagr101@gmail.com>
2020-09-06 16:42:14 -0300

    New: Delete product function

--- apirules/urls.py
+++ apirules/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from apirules.views import LoginView, Createuser, Product, ProductEdit, Pontuar
+from apirules.views import LoginView, Createuser, Product, ProductEdit, Pontuar, ProductEx
 
 app_name = 'dados'
 
@@ -9,6 +9,7 @@ urlpatterns = [
     path("cadastro", Createuser.as_view(), name="cadastro"),
     path("produtos", Product.as_view(), name="produto"),
     path("editarproduto", ProductEdit.as_view(), name="editarproduto"),
+    path("excluirproduto", ProductEx.as_view(), name="excluirproduto"),
     path("pontuar", Pontuar.as_view(), name="pontuar")
 
 ]
